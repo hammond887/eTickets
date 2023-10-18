50c55b87e16b9bbd043f260879cc2a9b3b5e3e91
Jonathan Hammond <jonathan.hammond@isitetv.com>
2023-10-18 11:19:36 +0100

    add index views

---
+++ aTickets/Models/Actor.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace aTickets.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Profile Picture")]
+        public string ProfilePictureURL { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName{ get; set; }
+        [Display(Name = "Biography")]
+        public string Bio{ get; set; }
+        //relationships
+        public List<Actor_Movie> Actor_Movies { get; set; }
+    }
+}
--- aTickets/Program.cs
+++ aTickets/Program.cs
@@ -1,9 +1,17 @@
+using aTickets.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString(
+    "DefaultConnectionString")));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+//seed db
+AppDbInitializer.Seed(app);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -23,5 +31,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
 
 app.Run();
